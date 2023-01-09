a85320998c56a4f6b2b6d83773e3e5936592fa1b
Reva Artem <revaartem2008@gmail.com>
2023-01-09 12:04:32 +0200

    Re-write homework according to the task

--- Prog_HOMEWORKS/Homework_OOP/homework_prog_oop_generators/cubes_generator.py
+++ Prog_HOMEWORKS/Homework_OOP/homework_prog_oop_generators/cubes_generator.py
@@ -1,15 +1,4 @@
 
-def cubes_generator(stop: int):
-    """
-    Generator for getting all numbers from 2 to "stop" in the third degree.
-
-    :param stop: Generator stop generating sequence when current number become bigger than "stop".
-    :return: Number ** 3
-    """
-    number = 2
-
-    while True:
-        if number >= stop:
-            break
-        yield number ** 3
-        number += 1
+num = int(input('Number: '))
+expression = (item ** 3 for item in range(2, num))
+res = list(expression)
