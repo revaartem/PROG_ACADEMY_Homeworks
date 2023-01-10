746eb4947d0e8a205832fadc63d166b29a282eb8
Reva Artem <revaartem2008@gmail.com>
2023-01-10 12:09:25 +0200

    Add homework

---
+++ Prog_HOMEWORKS/Homework_OOP/homework_prog_oop_closure and generators/task_3.py
@@ -0,0 +1,36 @@
+from typing import Callable
+
+
+def sum_func(func: Callable[[int], int]):
+    """
+    The function takes another function as an argument and
+    returns the sum of the elements of the list processed
+    by the given function.
+
+    :param func: Function to process the list.
+    :return: Sum of processed list.
+    """
+    numbers = list(range(1, 11))
+
+    processed_numbers = []
+
+    for number in numbers:
+        processed_numbers.append(func(number))
+
+    return sum(processed_numbers)
+
+
+def cubes(num: int):
+    """
+    Raises number to the 3rd power.
+
+    :param num: Number to process.
+    :return: Number ** 3.
+    """
+    num **= 3
+    return num
+
+
+if __name__ == "__main__":
+    test = sum_func(cubes)
+    print(test)
