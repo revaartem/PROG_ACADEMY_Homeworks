0ae6b74a1624dccfeed74eef6eaeffd325a1e66c
Reva Artem <revaartem2008@gmail.com>
2023-01-24 11:14:59 +0200

    Correct task 2

--- Prog_HOMEWORKS/homework_django/homework_regular_expressions/task_2.py
+++ Prog_HOMEWORKS/homework_django/homework_regular_expressions/task_2.py
@@ -1,13 +1,21 @@
 
 import re
 
-import re
+regex = r"^(\d{4}(\s|-)?){4}$"
 
-regex = r"(\d{4}(\s|-)?){4}"
+test_str = "6666666666666666"
+test_str_1 = "6666 6666 6666 6666"
+test_str_2 = "6666-6666-6666-6666"
 
-test_str = "6666 6666 6666 6666, 6666-6666-6666-6666, 6666666666666666"
-
-matches = re.finditer(regex, test_str, re.MULTILINE)
+matches = re.finditer(regex, test_str)
+matches_1 = re.finditer(regex, test_str_1)
+matches_2 = re.finditer(regex, test_str_2)
 
 for i in matches:
+    print(i)
+
+for i in matches_1:
+    print(i)
+
+for i in matches_2:
     print(i)
