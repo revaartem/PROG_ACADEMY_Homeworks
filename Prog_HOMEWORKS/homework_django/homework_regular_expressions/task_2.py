82b68b803d2d305caa30bee9989e1ae91e5e1a73
Reva Artem <revaartem2008@gmail.com>
2023-01-24 11:18:29 +0200

    Correct task 2

--- Prog_HOMEWORKS/homework_django/homework_regular_expressions/task_2.py
+++ Prog_HOMEWORKS/homework_django/homework_regular_expressions/task_2.py
@@ -7,15 +7,10 @@ test_str = "6666666666666666"
 test_str_1 = "6666 6666 6666 6666"
 test_str_2 = "6666-6666-6666-6666"
 
-matches = re.finditer(regex, test_str)
-matches_1 = re.finditer(regex, test_str_1)
-matches_2 = re.finditer(regex, test_str_2)
+matches = re.search(regex, test_str)
+matches_1 = re.search(regex, test_str_1)
+matches_2 = re.search(regex, test_str_2)
 
-for i in matches:
-    print(i)
-
-for i in matches_1:
-    print(i)
-
-for i in matches_2:
-    print(i)
+print(matches)
+print(matches_1)
+print(matches_2)
